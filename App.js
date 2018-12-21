a3d7bfe19b5d0537344627b67abf537a3a46af29
Jason C. Leach <jason.leach@fullboar.ca>
2018-12-20 16:45:10 -0800

    Create store and provider wrapper

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import Main from './src/components/Main';
+import configureStore from './src/configureStore';
+
+const store = configureStore();
 
 export default class App extends React.Component {
   render() {
-    return <Main />;
+    return (
+      <Provider store={store}>
+        <Main />
+      </Provider>
+    );
   }
 }
